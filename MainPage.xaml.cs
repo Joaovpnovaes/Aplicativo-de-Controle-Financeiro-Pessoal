083ce8c8273e2031c2acc643ad7258954bf3b6a5
beckyyz <rebeca18andradegarcia@gmail.com>
2024-11-03 22:10:19 -0300

    03/11

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,30 @@
-﻿namespace GerenciamentoFinanceiroPessoal
+﻿namespace GerenciamentoFinanceiroPessoal;
+
+public partial class UserRegistrationPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public UserRegistrationPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
 
+    private void OnCadastrarClicked(object sender, EventArgs e)
+    {
+        string nome = NomeEntry.Text;
+        string email = EmailEntry.Text;
+        string senha = SenhaEntry.Text;
+        string confirmarSenha = ConfirmarSenhaEntry.Text;
+
+        // Verifica se a senha e a confirmação coincidem
+        if (senha == confirmarSenha)
+        {
+            // Aqui você poderia salvar o cadastro ou realizar outra ação
+            ResultadoLabel.Text = "Cadastro realizado com sucesso!";
+            ResultadoLabel.TextColor = Colors.Green;
+        }
+        else
+        {
+            ResultadoLabel.Text = "As senhas não coincidem. Tente novamente.";
+            ResultadoLabel.TextColor = Colors.Red;
+        }
+    }
 }
